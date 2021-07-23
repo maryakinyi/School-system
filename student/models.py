19d34726e7e3b67850505bb59594518d575afe2b
maryakinyi <owuorakinyimary@gmail.com>
2021-07-23 19:30:10 +0300

    First commit

--- student/models.py
+++ student/models.py
@@ -6,3 +6,10 @@ class Student(models.Model):
         last_name=models.CharField(max_length=10)
         age=models.PositiveIntegerField()
         date_of_birth=models.DateField()
+        mentor_name=models.CharField(max_length=20)
+        big_sister=models.CharField(max_length=20)
+        passport_front=models.ImageField()
+        passport_back=models.ImageField()
+        gender=models.CharField(max_length=6)
+        guardian=models.CharField(max_length=10)
+        
--- student/views.py
+++ student/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .forms import StudentRegistrationForm
 
 # Create your views here.
+def register_student(request):
+        forms=StudentRegistrationForm()
+        return render(request,"register_student.html",{"forms":forms})
